cbceb58319b913c7aa6fdf66f85d8d730bb61866
Patrik Axelsson <patrik.bo@gmail.com>
2016-08-15 01:22:55 +0200

    Use timer output compare interrupt to handle handshake end.

--- amiga_keyb_if.c
+++ amiga_keyb_if.c
@@ -1,6 +1,5 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <util/delay.h>
 #include <stdbool.h>
 
 #define KEYB_IN PIND
@@ -21,9 +20,7 @@ ISR (PCINT3_vect) {
 		tmpData |= ((bit_is_clear(KEYB_IN, KEYB_DAT) ? 1 : 0) << bitPos);
 		if(7 == bitPos) {
 			keyboardData = tmpData;
-			if(0 != changeCallback) {
-				changeCallback(tmpData & 0x7f, tmpData & 0x80);
-			}
+			changeCallback(tmpData & 0x7f, tmpData & 0x80);
 			tmpData = 0x00;
 		}
 		bitPos--;
@@ -33,19 +30,37 @@ ISR (PCINT3_vect) {
 		if (6 == bitPos) {
 			KEYB_OUT &= ~_BV(KEYB_DAT); // Disable pull-up and set to zero/low if used for output
 			KEYB_DDR |= _BV(KEYB_DAT); // Set to output
-			_delay_us(85);
-			KEYB_DDR &= ~_BV(KEYB_DAT); // Set to input
-			KEYB_OUT |= _BV(KEYB_DAT); // Enable pull-up
+
+			// Let timer0 compare interrupt end handshake in 85us
+			TCNT0 = 0;
+			OCR0A = 85;
+			TIMSK0 |= _BV(OCIE0A);
 		}
 	}
 }
 
+ISR (TIMER0_COMPA_vect) {
+	KEYB_DDR &= ~_BV(KEYB_DAT); // Set to input
+	KEYB_OUT |= _BV(KEYB_DAT); // Enable pull-up
+	TIMSK0 &= ~_BV(OCIE0A);
+}
+
+static void dummyChangeCallback(uint8_t amigaKey, bool up) {
+}
+
 void amiga_keyb_if_init() {
+	changeCallback = dummyChangeCallback;
+
 	KEYB_DDR &= ~KEYB_MASK;
 	KEYB_OUT |= KEYB_MASK; //Pullup
 
 	PCICR |= _BV(PCIE3);
 	PCMSK3 |= _BV(PCINT26);
+
+	// Setup timer0 for use for handshake wait
+	TCCR0A = _BV(1 << WGM01); // Enable output compare mode
+	TCCR0B = _BV(1 << CS01); // Set prescaler clk/8 - counts up once per microsecond at 8MHz
+	
 	sei();
 }
 
