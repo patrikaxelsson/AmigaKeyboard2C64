6753a13845a7c4622ca8391fed12c6a8e1e2bd37
Patrik Axelsson <patrik.bo@gmail.com>
2016-08-15 14:25:51 +0200

    Changed keyboard handling to use state machine, fixing timer interrupt handshake end issues.

--- amiga_keyb_if.c
+++ amiga_keyb_if.c
@@ -1,6 +1,9 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdbool.h>
+#include <stdio.h>
+
+#include "amiga_keyb_codes.h"
 
 #define KEYB_IN PIND
 #define KEYB_OUT PORTD
@@ -12,37 +15,54 @@
 static volatile uint8_t keyboardData = 0xff;
 static void (*changeCallback)(uint8_t, bool) = 0;
 
+static enum State {
+	State_Data,
+	State_Handshake,
+	//State_Reset,
+} state = State_Data;
+
+static uint8_t bitPos = 6;
 ISR (PCINT3_vect) {
 	static uint8_t tmpData = 0x00;
-	static uint8_t bitPos = 6;
 
-	if (bit_is_clear(KEYB_IN, KEYB_CLK)) {
-		tmpData |= ((bit_is_clear(KEYB_IN, KEYB_DAT) ? 1 : 0) << bitPos);
-		if(7 == bitPos) {
-			keyboardData = tmpData;
-			changeCallback(tmpData & 0x7f, tmpData & 0x80);
-			tmpData = 0x00;
-		}
-		bitPos--;
-		bitPos &= 7; // Wrap -> 7
-	}
-	else {
-		if (6 == bitPos) {
-			KEYB_OUT &= ~_BV(KEYB_DAT); // Disable pull-up and set to zero/low if used for output
-			KEYB_DDR |= _BV(KEYB_DAT); // Set to output
-
-			// Let timer0 compare interrupt end handshake in 85us
-			TCNT0 = 0;
-			OCR0A = 85;
-			TIMSK0 |= _BV(OCIE0A);
-		}
+	//PORTB = ~_BV(bitPos);
+	switch(state) {
+		case State_Data:
+			if (bit_is_clear(KEYB_IN, KEYB_CLK)) {
+				tmpData |= ((bit_is_clear(KEYB_IN, KEYB_DAT) ? 1 : 0) << bitPos);
+				if(7 == bitPos) {
+					keyboardData = tmpData;
+					changeCallback(tmpData & 0x7f, tmpData & 0x80);
+					tmpData = 0x00;
+					state = State_Handshake;
+				}
+				bitPos--;
+				bitPos &= 7; // Wrap -> 7
+			}
+			break;
+		case State_Handshake:
+			if (bit_is_set(KEYB_IN, KEYB_CLK)) {
+				//PORTB = 0x00;
+				KEYB_OUT &= ~_BV(KEYB_DAT); // Disable pull-up and set to zero/low if used for output
+				KEYB_DDR |= _BV(KEYB_DAT); // Set to output
+	
+				// Let timer0 compare interrupt end handshake in 85us
+				TCNT0 = 0;
+				OCR0A = 85;
+				TIMSK0 |= _BV(OCIE0A);
+			}
+			break;
 	}
 }
 
 ISR (TIMER0_COMPA_vect) {
-	KEYB_DDR &= ~_BV(KEYB_DAT); // Set to input
-	KEYB_OUT |= _BV(KEYB_DAT); // Enable pull-up
-	TIMSK0 &= ~_BV(OCIE0A);
+	if(State_Handshake == state) {
+		TIMSK0 &= ~_BV(OCIE0A);
+		KEYB_DDR &= ~_BV(KEYB_DAT); // Set to input
+		KEYB_OUT |= _BV(KEYB_DAT); // Enable pull-up
+		state = State_Data;
+		//PORTB = 0xff;
+	}
 }
 
 static void dummyChangeCallback(uint8_t amigaKey, bool up) {
@@ -58,9 +78,12 @@ void amiga_keyb_if_init() {
 	PCMSK3 |= _BV(PCINT26);
 
 	// Setup timer0 for use for handshake wait
-	TCCR0A = _BV(1 << WGM01); // Enable output compare mode
-	TCCR0B = _BV(1 << CS01); // Set prescaler clk/8 - counts up once per microsecond at 8MHz
+	TCCR0A = _BV(WGM01); // Enable output compare mode
+	TCCR0B = _BV(CS00); // Set prescaler clk/8 - counts up once per microsecond at 8MH
 	
+	DDRB = 0xff;
+	PORTB=0x81;
+
 	sei();
 }
 
